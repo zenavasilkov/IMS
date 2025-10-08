b1f6b98433408caa0abdd983b6e187fe1f1b21f6
zenavasilkov <zenavasilkov6@gmail.com>
2025-10-08 23:43:56 +0300

    Add Generic Service

---
+++ IMS/IMS.BLL/Services/Interfaces/IService.cs
@@ -0,0 +1,24 @@
+﻿using IMS.BLL.Models;
+using IMS.DAL.Entities;
+using Shared.Pagination;
+using System.Linq.Expressions;
+
+namespace IMS.BLL.Services.Interfaces;
+
+public interface IService<TModel, TEntity> 
+    where TModel : ModelBase 
+    where TEntity : EntityBase
+{
+    Task<List<TModel>> GetAllAsync(Expression<Func<TEntity, bool>>? predicate, bool trackChanges = false, CancellationToken cancellationToken = default);
+
+    Task<PagedList<TModel>> GetPagedAsync(Expression<Func<TEntity, bool>>? predicate,
+         PaginationParameters paginationParameters, bool trachChanges = false, CancellationToken cancellationTokent = default);
+
+    Task<TModel?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task<TModel> CreateAsync(TModel model, CancellationToken cancellationToken = default);
+
+    Task<TModel> UpdateAsync(TModel model, CancellationToken cancellationToken = default);
+
+    Task<bool> DeleteAsync(TModel model, CancellationToken cancellationToken = default);
+}
