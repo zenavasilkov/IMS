e278cc6d95b143e03c32808f9774638c3e2d2a6e
zenavasilkov <zenavasilkov6@gmail.com>
2025-10-09 13:42:20 +0300

    Remove unnecessary files and added extension method

---
+++ IMS/IMS.BLL/Extensions/MappingProblemResolvingExtension.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using IMS.BLL.Models;
+
+namespace IMS.BLL.Extensions;
+
+public static class MappingProblemResolvingExtension
+{
+
+    public static InternshipModel AddReferences(this InternshipModel internship, IMapper mapper)
+    {
+        var model = mapper.Map<InternshipModel>(internship);
+
+        model.Intern.Mentor = model.Mentor;
+        model.Mentor.Inters.Add(model.Intern);
+        model.Intern.HRManager = model.HumanResourcesManager;
+        model.Mentor.HRManager = model.HumanResourcesManager;
+        model.HumanResourcesManager.Interships.Add(model);
+        model.Intern.Internship = model;
+
+        return model;
+    }
+}
--- IMS/IMS.BLL/Services/Interfaces/IInternService.cs
+++
@@ -1,9 +0,0 @@
-﻿using IMS.BLL.Models;
-using IMS.DAL.Entities;
-
-namespace IMS.BLL.Services.Interfaces;
-
-public interface IInternService : IService<InternModel, User>
-{
-
-}
