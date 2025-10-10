2927e2145b4a9d11f978ee1e226c23d7882de927
zenavasilkov <zenavasilkov6@gmail.com>
2025-10-10 13:36:28 +0300

    Add User DTO

---
+++ IMS/IMS.Presentation/DTOs/AddUserDTO.cs
@@ -0,0 +1,18 @@
+﻿using Shared.Enums;
+
+namespace IMS.Presentation.DTOs;
+
+public class AddUserDTO
+{
+    public required string Email { get; set; }
+
+    public required string PhoneNumber { get; set; }
+
+    public required string Firstname { get; set; }
+
+    public required string Lastname { get; set; }
+
+    public string? Patronymic { get; set; }
+
+    public required Role Role { get; set; }
+}
---
+++ IMS/IMS.Presentation/DTOs/UpdateUserDTO.cs
@@ -0,0 +1,20 @@
+﻿using Shared.Enums;
+
+namespace IMS.Presentation.DTOs;
+
+public class UpdateUserDTO
+{
+    public required Guid Id { get; set; }
+
+    public required string Email { get; set; }
+
+    public required string PhoneNumber { get; set; }
+
+    public required string Firstname { get; set; }
+
+    public required string Lastname { get; set; }
+
+    public string? Patronymic { get; set; }
+
+    public required Role Role { get; set; }
+}
