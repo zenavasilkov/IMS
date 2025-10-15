d8374a483e100a7e8dffd6ea1475925d88fe0551
zenavasilkov <zenavasilkov6@gmail.com>
2025-10-15 15:28:13 +0300

    Add Swagger

--- IMS/IMS.Presentation/Program.cs
+++ IMS/IMS.Presentation/Program.cs
@@ -1,4 +1,5 @@
 using IMS.BLL.Extensions;
+using Microsoft.AspNetCore.Builder;
 
 namespace IMS.Presentation
 {
@@ -10,14 +11,24 @@ namespace IMS.Presentation
              
             builder.Services.AddBusinessLayerDedendencies(builder.Configuration);
 
-            builder.Services.AddControllers(); 
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddOpenApi();
+
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build(); 
              
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "IMS API v1");
+                    options.RoutePrefix = string.Empty;
+                }); 
             }
 
             app.UseHttpsRedirection();
