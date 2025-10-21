2149333851a398d320baf2fded1df33bb9284401
zenavasilkov <zenavasilkov6@gmail.com>
2025-10-21 14:04:15 +0300

    Make method static

--- IMS/IMS.DAL/Repositories/UserRepository.cs
+++ IMS/IMS.DAL/Repositories/UserRepository.cs
@@ -5,80 +5,80 @@ using Microsoft.EntityFrameworkCore;
 using Shared.Enums;
 using Shared.Pagination;
 
-namespace IMS.DAL.Repositories;
-
-public class UserRepository(IMSDbContext context, IUserFilterBuilder filterBuilder) 
-    : Repository<User>(context), IUserRepository
+namespace IMS.DAL.Repositories
 {
-    private readonly DbSet<User> _users = context.Set<User>();
-
-    public async Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken = default)
+    public class UserRepository(IMSDbContext context, IUserFilterBuilder filterBuilder) 
+        : Repository<User>(context), IUserRepository
     {
-        var exists = await _users.AnyAsync(u => u.Email == email, cancellationToken);
-
-        return exists;
-    }
-
-    public async Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default)
-    {
-        var query = _users
-            .AsNoTracking()
-            .Include(u => u.Internships)
-            .AsQueryable();
-
-        var user = await filterBuilder
-            .WithEmail(email)
-            .Build(query)
-            .FirstOrDefaultAsync(cancellationToken);
-
-        return user;
-    }
-
-    public async Task<PagedList<User>> GetAllAsync(
-        PaginationParameters paginationParameters,
-        UserFilteringParameters filter,
-        UserSortingParameter sorter,
-        bool trackChanges = false,
-        CancellationToken cancellationToken = default)
-    {
-        var query = _users.AsQueryable();
-
-        query = trackChanges ? query : query.AsNoTracking();
-
-        if(filter.Role != null) query = query.Where(u => u.Role == filter.Role);
-        if(filter.FirstName != null) query = query.Where(u => u.Firstname == filter.FirstName);
-        if(filter.LastName != null) query = query.Where(u => u.Lastname == filter.LastName);
-
-        query = AplySortingOption(query, sorter);
-
-        var users = await query
-            .Skip((paginationParameters.PageNumber - 1) * paginationParameters.PageSize)
-            .Take(paginationParameters.PageSize)
-            .ToListAsync(cancellationToken);
-
-        var totalCount = await query.CountAsync(cancellationToken);
-
-        var pagedList = new PagedList<User>(users, paginationParameters.PageNumber, 
-            paginationParameters.PageSize, totalCount);
-
-        return pagedList;
-    }
-
-    private static IQueryable<User> AplySortingOption(IQueryable<User> query, UserSortingParameter sortingParameter)
-    {
-        query = sortingParameter switch
+        private readonly DbSet<User> _users = context.Set<User>();
+        public async Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken = default)
         {
-            UserSortingParameter.AscendingId => query.OrderBy(u => u.Id),
-            UserSortingParameter.AscendingFirstName => query.OrderBy(u => u.Firstname),
-            UserSortingParameter.AscendingLastName => query.OrderBy(u => u.Lastname),
-            UserSortingParameter.AscendingRole => query.OrderBy(u => u.Role),
-            UserSortingParameter.DescendingId => query.OrderByDescending(u => u.Id),
-            UserSortingParameter.DescendingFirstName => query.OrderByDescending(u => u.Firstname),
-            UserSortingParameter.DescendingLastName => query.OrderByDescending(u => u.Lastname),
-            UserSortingParameter.DescendingRole => query.OrderByDescending(u => u.Role),
-            _ => query
-        };
+            var exists = await _users.AnyAsync(u => u.Email == email, cancellationToken);
 
-        return query;
+            return exists;
+        }
+
+        public async Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default)
+        {
+            var query = _users
+                .AsNoTracking()
+                .Include(u => u.Internships)
+                .AsQueryable();
+
+            var user = await filterBuilder
+                .WithEmail(email)
+                .Build(query)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            return user;
+        }
+
+        public async Task<PagedList<User>> GetAllAsync(
+            PaginationParameters paginationParameters,
+            UserFilteringParameters filter,
+            UserSortingParameter sorter,
+            bool trackChanges = false,
+            CancellationToken cancellationToken = default)
+        {
+            var query = _users.AsQueryable();
+
+            query = trackChanges ? query : query.AsNoTracking();
+
+            if(filter.Role != null) query = query.Where(u => u.Role == filter.Role);
+            if(filter.FirstName != null) query = query.Where(u => u.Firstname == filter.FirstName);
+            if(filter.LastName != null) query = query.Where(u => u.Lastname == filter.LastName);
+
+            query = AplySortingOption(query, sorter);
+
+            var users = await query
+                .Skip((paginationParameters.PageNumber - 1) * paginationParameters.PageSize)
+                .Take(paginationParameters.PageSize)
+                .ToListAsync(cancellationToken);
+
+            var totalCount = await query.CountAsync(cancellationToken);
+
+            var pagedList = new PagedList<User>(users, paginationParameters.PageNumber, 
+                paginationParameters.PageSize, totalCount);
+
+            return pagedList;
+        }
+
+        private static IQueryable<User> AplySortingOption(IQueryable<User> query, UserSortingParameter sortingParameter)
+        {
+            query = sortingParameter switch
+            {
+                UserSortingParameter.AscendingId => query.OrderBy(u => u.Id),
+                UserSortingParameter.AscendingFirstName => query.OrderBy(u => u.Firstname),
+                UserSortingParameter.AscendingLastName => query.OrderBy(u => u.Lastname),
+                UserSortingParameter.AscendingRole => query.OrderBy(u => u.Role),
+                UserSortingParameter.DescendingId => query.OrderByDescending(u => u.Id),
+                UserSortingParameter.DescendingFirstName => query.OrderByDescending(u => u.Firstname),
+                UserSortingParameter.DescendingLastName => query.OrderByDescending(u => u.Lastname),
+                UserSortingParameter.DescendingRole => query.OrderByDescending(u => u.Role),
+                _ => query
+            };
+
+            return query;
+        }
     }
 }
