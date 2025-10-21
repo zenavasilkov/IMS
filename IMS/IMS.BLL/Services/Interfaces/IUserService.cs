affd9c91f572dc4d8377d1edbbd5a2736d1896bf
zenavasilkov <zenavasilkov6@gmail.com>
2025-10-21 14:11:41 +0300

    Return to the previous state

--- IMS/IMS.BLL/Services/Interfaces/IUserService.cs
+++ IMS/IMS.BLL/Services/Interfaces/IUserService.cs
@@ -6,7 +6,8 @@ using Shared.Pagination;
 namespace IMS.BLL.Services.Interfaces;
 
 public interface IUserService : IService<UserModel, User>
-{    Task<List<UserModel>> GetUsersByRoleAsync(Role role, bool trackCanges = false, CancellationToken cancellationToken = default);
+{
+    Task<List<UserModel>> GetUsersByRoleAsync(Role role, bool trackCanges = false, CancellationToken cancellationToken = default);
 
     public Task<PagedList<UserModel>> GetUsersAsync(PaginationParameters paginationParameters, 
         UserFilteringParameters filter, UserSortingParameter sorter,CancellationToken cancellationToken = default);
