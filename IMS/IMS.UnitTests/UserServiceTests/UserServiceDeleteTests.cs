dac29b940b1fe38b1b7d9fbc436e1a08a7ecb991
zenavasilkov <zenavasilkov6@gmail.com>
2025-10-21 13:42:06 +0300

    Add delete test

--- IMS/IMS.UnitTests/UserServiceTests/UserServiceDeleteTests.cs
+++ IMS/IMS.UnitTests/UserServiceTests/UserServiceDeleteTests.cs
@@ -2,4 +2,21 @@
 
 public class UserServiceDeleteTests : UserServiceTestsBase
 {
+    [Fact]
+    public async Task DeleteAsync_DeletesUser_WhenUserExists()
+    {
+        //Arrange
+        var userModel = Fixture.Create<UserModel>();
+        var mappedUserEntity = Fixture.Create<User>();
+
+        MapperMock.Setup(m => m.Map<User>(userModel)).Returns(mappedUserEntity);
+        UserRepositoryMock.Setup(r => r.DeleteAsync(mappedUserEntity, default));
+
+        //Act
+        await UserService.DeleteAsync(userModel);
+
+        //Assert
+        MapperMock.Verify(m => m.Map<User>(userModel), Times.Once());
+        UserRepositoryMock.Verify(r => r.DeleteAsync(mappedUserEntity, default), Times.Once());
+    }
 }
