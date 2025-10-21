f0325a0fb56bf0629eebb412677495d1d6b8bf96
zenavasilkov <zenavasilkov6@gmail.com>
2025-10-21 14:19:37 +0300

    Remove unused methods and duplicating logic

--- IMS/IMS.BLL/Services/Interfaces/IUserService.cs
+++ IMS/IMS.BLL/Services/Interfaces/IUserService.cs
@@ -7,8 +7,6 @@ namespace IMS.BLL.Services.Interfaces;
 
 public interface IUserService : IService<UserModel, User>
 {
-    Task<List<UserModel>> GetUsersByRoleAsync(Role role, bool trackCanges = false, CancellationToken cancellationToken = default);
-
     public Task<PagedList<UserModel>> GetUsersAsync(PaginationParameters paginationParameters, 
         UserFilteringParameters filter, UserSortingParameter sorter,CancellationToken cancellationToken = default);
 }
