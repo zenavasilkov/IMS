4d6ff7dc377679d9e4b3ade03b92aedd9c079b78
zenavasilkov <zenavasilkov6@gmail.com>
2025-10-22 21:54:04 +0300

    Add global using

---
+++ IMS/IMS.IntegrationTests/GlobalUsing.cs
@@ -0,0 +1,14 @@
+﻿//Packages
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Storage;
+global using Microsoft.Extensions.DependencyInjection;
+
+//DAL
+global using IMS.DAL;
+
+//API
+global using IMS.Presentation;
+
+
