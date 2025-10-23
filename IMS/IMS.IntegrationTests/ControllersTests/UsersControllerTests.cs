8b9c812b048a33598efd029619b4cf38a12627c8
zenavasilkov <zenavasilkov6@gmail.com>
2025-10-23 12:09:42 +0300

    Fix behavior

--- IMS/IMS.IntegrationTests/ControllersTests/UsersControllerTests.cs
+++ IMS/IMS.IntegrationTests/ControllersTests/UsersControllerTests.cs
@@ -1,4 +1,7 @@
-﻿namespace IMS.IntegrationTests.ControllersTests;
+﻿using IMS.Presentation.DTOs.GetDTO;
+using Shared.Pagination;
+
+namespace IMS.IntegrationTests.ControllersTests;
 
 public class UsersControllerTests(CustomWebApplicationFactory factory) : TestHelperBase(factory)
 {
@@ -16,5 +19,45 @@ public class UsersControllerTests(CustomWebApplicationFactory factory) : TestHel
 
         //Assert
         response.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GetById_ShouldReturnNotFoundStatus_WhenUserDoesNotExist()
+    {
+        //Arrange
+
+        //Act
+        var response = await Client.GetAsync($"{ApiRoutes.Users.Base}/{Guid.NewGuid()}");
+
+        //Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task GetAll_ShouldReturnPaginatedList()
+    {
+        //Arrange
+        var users = new[]
+        {
+            TestDataHelper.CreateUser(firstname: "John", lastname: "Smith", role: Role.Mentor),
+            TestDataHelper.CreateUser(firstname: "Alice", lastname: "Brown", role: Role.Intern),
+            TestDataHelper.CreateUser(firstname: "Michael", lastname: "Davis", role: Role.HRManager)
+        };
+
+        await AddEntitiesAsync(users);
+
+        var url = $"{ApiRoutes.Users.Base}?PageNumber=1&PageSize=2";
+
+        //Act
+        var response = await Client.GetAsync(url);
+
+        //Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        var result = Deserialize<PagedList<UserDTO>>(await response.Content.ReadAsStringAsync());
+        result.ShouldNotBeNull();
+
+        result.Items.Count.ShouldBe(3);
+        result.TotalCount.ShouldBe(3);
     }
 }
