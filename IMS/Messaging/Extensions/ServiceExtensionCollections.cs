d3503fbe2566c39b646e08de601d064f3d4d1bb6
zenavasilkov <zenavasilkov6@gmail.com>
2025-10-31 11:13:48 +0300

    Update NugetPackage

---
+++ IMS/Messaging/Extensions/ServiceExtensionCollections.cs
@@ -0,0 +1,39 @@
+﻿using IMS.NotificationsCore.Options;
+using IMS.NotificationsCore.Services;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace IMS.NotificationsCore.Extensions;
+
+public static class ServiceExtensionCollections
+{
+    public static IServiceCollection AddNotifications(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<RabbitMqOptions>(configuration.GetSection(RabbitMqOptions.SectionName));
+
+        services.AddScoped<IMessageService, MessageService>();
+
+        services.AddMassTransit(busConfigurator =>
+        {
+            busConfigurator.SetKebabCaseEndpointNameFormatter();
+
+            busConfigurator.UsingRabbitMq((context, rabbitMqConfigurator) =>
+            {
+                var rabbitMqOptions = context.GetRequiredService<IOptions<RabbitMqOptions>>().Value;
+
+                rabbitMqConfigurator.Host(rabbitMqOptions.HostName, rabbitMqOptions.VirtualHostName, configure =>
+                {
+                    configure.Username(rabbitMqOptions.UserName);
+                    configure.Password(rabbitMqOptions.Password);
+                });
+
+                rabbitMqConfigurator.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
+}
