82014314dea5fe878b69984e712e8ab3b1d682be
zenavasilkov <zenavasilkov6@gmail.com>
2025-10-31 12:45:28 +0300

    Add constants

---
+++ NotificationService/NotificationService/Constants/SubjectConstats.cs
@@ -0,0 +1,9 @@
+﻿namespace NotificationService.Constants;
+
+public static class SubjectConstats
+{
+    public const string UserCreated = "You have new account in IMS";
+    public const string FeedbackCreated = "You recieved feedback on assigment '{TicketTitle}'";
+    public const string TicketCreated = "You have new assignment in IMS";
+    public const string TicketStatusChanged = "Assignment '{TicketTitle}' status has been changed from '{OldStatust} to '{NewStatus}'";
+}
