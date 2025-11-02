beff51eca7e19b44a9c906bc1382df8e8d88db46
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-02 17:24:45 +0300

    Add options

---
+++ NotificationService/NotificationService/Options/RabbitMqOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace NotificationService.Options;
+
+public class RabbitMqOptions
+{
+    public const string SectionName = "RabbitMQ";
+
+    public string HostName { get; set; } = default!;
+    public string VirtualHostName { get; set; } = default!;
+    public string UserName { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
