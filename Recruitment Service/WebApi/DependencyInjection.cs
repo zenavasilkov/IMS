fa9460adb749006987b99af2f4732eb009b1dedf
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-04 18:42:37 +0300

    Add base structure

---
+++ Recruitment Service/WebApi/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Application;
+using Infrastructure;
+using Presentation;
+
+namespace WebApi;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDependencies(this IServiceCollection services)
+    {
+        services
+            .AddPresentation()
+            .AddInfrastructure()
+            .AddApplication();
+
+        return services;
+    }
+}
