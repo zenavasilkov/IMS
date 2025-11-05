fa671c7eb6e951dc32365397ed1915ed90bcbdc7
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-05 13:54:24 +0300

    Add base structure

---
+++ Recruitment Service/WebApi/Program.cs
@@ -0,0 +1,33 @@
+using Serilog;
+
+namespace WebApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration)
+            .CreateLogger();
+
+        builder.Host.UseSerilog();
+
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+         
+        app.UseHttpsRedirection(); 
+        app.UseAuthorization(); 
+        app.MapControllers();
+
+        app.Run();
+    }
+}
