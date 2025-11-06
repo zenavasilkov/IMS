51c8d7ed7a68afde4e332b15628a8c0c50560d98
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-06 13:17:20 +0300

    Add contracts

---
+++ Recruitment Service/Domain/Contracts/Repositories/IGeneralRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Domain.Contracts.Repositories;
+
+public interface IGeneralRepository<T> : IGenericReadOnlyRepository<T> where T : Entity
+{
+    Task<T> CreateAsync(T entity, CancellationToken cancellationToken = default);
+
+    Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task UpdateAsync(CancellationToken cancellationToken = default);
+}
---
+++ Recruitment Service/Domain/Contracts/Repositories/IReadOnlyRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Pagination;
+using System.Linq.Expressions;
+
+namespace Domain.Contracts.Repositories;
+
+public interface IGenericReadOnlyRepository<T>
+    where T : Entity
+{
+    Task<PagedList<T>> FindByConditionAsync(
+        Expression<Func<T, bool>> expression,
+        PaginationParameters paginationParameters,
+        bool trackChanges = false,
+        CancellationToken cancellationToken = default);
+
+    Task<T?> FindByIdAsync(Guid id, bool trackChanges = true, CancellationToken cancellationToken = default);
+}
