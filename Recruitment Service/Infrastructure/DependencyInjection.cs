afff64e9e2c20d20e904ec1c431328bcf6a3222b
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-09 13:54:06 +0300

    Squashed commit of the following:
    commit 3b6257ee605771d96bf4be8381f103fcd0bbf794
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Sat Nov 8 23:03:23 2025 +0300
    
        Compare email ignoring case
    
    commit 7aeaa7f6ed47c1b6345816e4d1f965abe95294cc
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Sat Nov 8 22:01:24 2025 +0300
    
        Update repositories
    
    commit c5f093cca455559be70d0ac8ea7330258ad9b422
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Fri Nov 7 23:25:43 2025 +0300
    
        Update Infrastructure
    
    commit 08b80fe001da15e2dfe3604f49badb2aef25e418
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Fri Nov 7 18:32:24 2025 +0300
    
        Add-Infrastructure
    
    commit 5ee51b2a7c0882f99619b99f90b5d026c549a573
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Fri Nov 7 23:12:39 2025 +0300
    
        Update Domain
    
    commit c454cd0fbfaa0ff48e940460890cd1bc8308707c
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Fri Nov 7 18:30:42 2025 +0300
    
        Update Domain
    
    commit f0375f4aa70822e32df7ad5cd1b94a22124c262d
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Thu Nov 6 17:38:53 2025 +0300
    
        Rename repositories
    
    commit 297d725e8a0586d88170622cb835abcb652f8a7c
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Thu Nov 6 17:05:57 2025 +0300
    
        Add empty constructor for EF
    
    commit bd3e25dbacef82b41fb704173e8f62a9838b4c7b
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Thu Nov 6 13:17:20 2025 +0300
    
        Add contracts
    
    commit 1adc73b117912bb4247d687705c99101cf5d3597
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Thu Nov 6 13:15:47 2025 +0300
    
        Revert "Add employees collection to department"
    
        This reverts commit 0b5536690bcbd77fcf1f92977bcd6f4cc098adeb.
    
    commit 0b5536690bcbd77fcf1f92977bcd6f4cc098adeb
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Thu Nov 6 12:14:34 2025 +0300
    
        Add employees collection to department
    
    commit fb5baff33346c8edf1257ace06ada9f2a4ab0f1e
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Thu Nov 6 11:27:00 2025 +0300
    
        Update feedback behavior
    
    commit dfe9038d3dae349048cc5e199d6813c1de27ac46
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Thu Nov 6 11:13:19 2025 +0300
    
        Add logic to entities
    
    commit dc3b7cc3f289994a05ed373faf84a5a755040c77
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Thu Nov 6 00:29:50 2025 +0300
    
        Add implicit casting
    
    commit d3701856baaec004666cdff960c172363bb8fee4
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Wed Nov 5 23:17:23 2025 +0300
    
        Remove nullability check for feedback
    
    commit 097d471910195ebdca9c2e0eda5896d56a0c6877
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Wed Nov 5 23:03:27 2025 +0300
    
        Update CI-pipeline
    
    commit d5f1fa8070cbc103db02d502a3da187c7f14a022
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Wed Nov 5 22:59:34 2025 +0300
    
        Update CI-Pipeline
    
    commit 8d3ff0deb5523b68170b0627dfa375f02ce5c796
    Author: zenavasilkov <zenavasilkov6@gmail.com>
    Date:   Wed Nov 5 22:04:08 2025 +0300
    
        Add entities

--- Recruitment Service/Infrastructure/DependencyInjection.cs
+++ Recruitment Service/Infrastructure/DependencyInjection.cs
@@ -1,11 +1,51 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Domain.Contracts.Repositories;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services
+            .AddDbContext(configuration)
+            .AddRepositories();
+
         return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services
+            .AddScoped(typeof(IGenericReadOnlyRepository<>), typeof(GenericReadOnlyRepository<>))
+            .AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>))
+            .AddScoped<IDepartmentRepository, DepartmentRepository>()
+            .AddScoped<ICandidateRepository, CandidateRepository>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<RecruitmentDbContext>(options => options.UseNpgsql(connectionString));
+
+        return services;
+    }
+
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<RecruitmentDbContext>();
+
+        if (dbContext.Database.IsRelational())
+        {
+            dbContext.Database.Migrate();
+        }
     }
 }
---
+++ Recruitment Service/Infrastructure/Repositories/DepartmentRepository.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Contracts.Repositories;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Pagination;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repositories;
+
+public class DepartmentRepository(RecruitmentDbContext context, IGenericRepository<Department> repository) : IDepartmentRepository
+{
+    public Task<Department> CreateAsync(Department entity, CancellationToken cancellationToken = default) =>
+        repository.CreateAsync(entity, cancellationToken);
+
+    public Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default) =>
+        repository.DeleteAsync(id, cancellationToken);
+
+    public Task<PagedList<Department>> GetByConditionAsync(Expression<Func<Department, bool>> expression,
+        PaginationParameters paginationParameters, bool trackChanges = false, CancellationToken cancellationToken = default) =>
+        repository.GetByConditionAsync(expression, paginationParameters, trackChanges, cancellationToken);
+
+    public Task<Department?> GetByIdAsync(Guid id, bool trackChanges = true, CancellationToken cancellationToken = default) =>
+        repository.GetByIdAsync(id, trackChanges, cancellationToken);
+
+    public async Task<Department?> GetByNameAsync(string name, bool trackChanges = true, CancellationToken cancellationToken = default)
+    {
+        var query = context.Departments.AsQueryable();
+
+        query = trackChanges ? query : query.AsNoTracking();
+            
+        var department = await query.FirstOrDefaultAsync(d => d.Name == name, cancellationToken);
+
+        return department;
+    }
+
+    public Task<Department> UpdateAsync(Department entity, CancellationToken cancellationToken = default) =>
+        repository.UpdateAsync(entity, cancellationToken);
+}
