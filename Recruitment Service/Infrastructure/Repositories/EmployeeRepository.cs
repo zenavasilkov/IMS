91fcce4d107b615faac378a5965773b99dc51626
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-09 12:33:47 +0300

    Add case unsensitive email comparison

--- Recruitment Service/Infrastructure/Repositories/EmployeeRepository.cs
+++ Recruitment Service/Infrastructure/Repositories/EmployeeRepository.cs
@@ -22,7 +22,7 @@ public class EmployeeRepository(IGenericRepository<Employee> repository, Recruit
 
         query = trackChanges ? query : query.AsNoTracking();
 
-        var employee = await query.FirstOrDefaultAsync(e => e.Email == email, cancellationToken);
+        var employee = await query.FirstOrDefaultAsync(e => e.Email.Equals(email, StringComparison.OrdinalIgnoreCase), cancellationToken);
 
         return employee;
     }
