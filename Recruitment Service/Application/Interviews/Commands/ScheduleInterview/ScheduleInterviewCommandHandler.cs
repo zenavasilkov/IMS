d6d9629e7cb07322a902887b49acfc602dd78aa9
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-10 15:07:42 +0300

    Divide method into two

--- Recruitment Service/Application/Interviews/Commands/ScheduleInterview/ScheduleInterviewCommandHandler.cs
+++ Recruitment Service/Application/Interviews/Commands/ScheduleInterview/ScheduleInterviewCommandHandler.cs
@@ -15,24 +15,30 @@ public class ScheduleInterviewCommandHandler(
 {
     public async Task<Result<Guid>> Handle(ScheduleInterviewCommand request, CancellationToken cancellationToken)
     {
-        var candidate = await candidateRepository.GetByIdAsync(request.CandidateId, false, cancellationToken);
-
-        if (candidate is null) return CandidateErrors.NotFound;
-
-        var interviewer = await employeeRepository.GetByIdAsync(request.InterviewerId, false, cancellationToken);
-
-        if (interviewer is null) return EmployeeErrors.NotFound;
-
-        var department = await departmentRepository.GetByIdAsync(request.DepartmentId, false, cancellationToken);
-
-        if (department is null) return DepartmentErrors.NotFound;
+        var result = await ValidateDependencies(request, cancellationToken);
+        if (result.IsFailure) return result.Error;
+        var (candidate, interviewer, department) = result.Value;
 
         var interview = Interview.Create(Guid.NewGuid(), candidate, interviewer, department, request.Type, request.ScheduledAt);
-
         if (interview.IsFailure) return interview.Error;
 
         await repository.CreateAsync(interview.Value, cancellationToken);
 
         return Result.Success(interview.Value.Id);
+    }
+
+    private async Task<Result<(Candidate, Employee, Department)>> ValidateDependencies(
+        ScheduleInterviewCommand request, CancellationToken cancellationToken)
+    {
+        var candidate = await candidateRepository.GetByIdAsync(request.CandidateId, false, cancellationToken);
+        if (candidate is null) return CandidateErrors.NotFound;
+
+        var interviewer = await employeeRepository.GetByIdAsync(request.InterviewerId, false, cancellationToken);
+        if (interviewer is null) return EmployeeErrors.NotFound;
+
+        var department = await departmentRepository.GetByIdAsync(request.DepartmentId, false, cancellationToken);
+        if (department is null) return DepartmentErrors.NotFound;
+
+        return Result.Success((candidate, interviewer, department));
     }
 }
