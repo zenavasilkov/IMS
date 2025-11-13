9f0bf694997d9a252daef536804b170334adf451
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-13 14:59:29 +0300

    Implement Outbox pattern

---
+++ Recruitment Service/Infrastructure/BackgroundJobs/ProcessOutboxMessagesJob.cs
@@ -0,0 +1,56 @@
+﻿using Infrastructure.Outbox;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Quartz;
+using RecruitmentNotifications.Messages;
+
+namespace Infrastructure.BackgroundJobs;
+
+[DisallowConcurrentExecution]
+internal class ProcessOutboxMessagesJob(RecruitmentDbContext dbContext,
+    IPublisher publisher, ILogger<ProcessOutboxMessagesJob> logger) : IJob
+{
+    private const int BatchSize = 20;
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        var messages = await dbContext.Set<OutboxMessage>()
+            .Where(m => m.ProcessedOnUtc == null)
+            .Take(BatchSize)
+            .ToListAsync(context.CancellationToken);
+
+        foreach (var message in messages)
+        {
+            Type? eventType = null;
+
+            var assembly = typeof(BaseEvent).Assembly;
+                
+            eventType = assembly.GetType(message.Type);
+
+            if (eventType == null)
+            {
+                logger.LogWarning("Could not find type {TypeName} for outbox message with Id {MessageId}." +
+                    " Assembly might not be loaded or name is incorrect.", message.Type, message.Id);
+
+                continue;
+            }
+
+            var domainEvent = JsonConvert.DeserializeObject(message.Content, eventType);
+
+            if (domainEvent is null)
+            {
+                logger.LogWarning("Failed to deserialize outbox message with Id {MessageId}", message.Id);
+
+                continue;
+            }
+
+            await publisher.Publish(domainEvent, context.CancellationToken);
+
+            message.ProcessedOnUtc = DateTime.UtcNow;
+        }
+
+        await dbContext.SaveChangesAsync(context.CancellationToken);
+    }
+}
---
+++ Recruitment Service/Infrastructure/Configuration/DepartmentConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static Infrastructure.Constants.TableNames;
+
+namespace Infrastructure.Configuration;
+
+internal class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+{
+    public void Configure(EntityTypeBuilder<Department> builder)
+    {
+        builder.ToTable(Departments);
+
+        builder.HasKey(d => d.Id);
+    }
+}
