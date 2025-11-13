83af44a4d5115a1bb1e549cb5e20ac6994f269d9
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-13 14:39:29 +0300

    Update .csproj file, add assembly reference

---
+++ Recruitment Service/Application/Interviews/Commands/CancelInterview/CancelInterviewNotificationHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using RecruitmentNotifications.Messages;
+using RecruitmentNotifications.Servises;
+
+namespace Application.Interviews.Commands.CancelInterview;
+
+public class CancelInterviewNotificationHandler(IMessageSender sender) : INotificationHandler<InterviewCancelledEvent>
+{
+    public Task Handle(InterviewCancelledEvent notification, CancellationToken cancellationToken) =>
+        sender.NotifyInterviewCancelled(notification, cancellationToken);
+}
