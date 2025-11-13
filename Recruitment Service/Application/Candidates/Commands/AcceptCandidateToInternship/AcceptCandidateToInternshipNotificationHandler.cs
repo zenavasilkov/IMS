925d4029197408d2f516fc81390cdce44e1767cb
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-13 14:36:44 +0300

    Add Notification Handlers

---
+++ Recruitment Service/Application/Candidates/Commands/AcceptCandidateToInternship/AcceptCandidateToInternshipNotificationHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using RecruitmentNotifications.Messages;
+using RecruitmentNotifications.Servises;
+
+namespace Application.Candidates.Commands.AcceptCandidateToInternship;
+
+internal class AcceptCandidateToInternshipNotificationHandler(IMessageSender sender) : INotificationHandler<CandidatePassedToInternshipEvent>
+{
+    public Task Handle(CandidatePassedToInternshipEvent notification, CancellationToken cancellationToken) =>
+        sender.NotifyCandidatePassedToInterview(notification, cancellationToken);
+}
---
+++ Recruitment Service/Application/Interviews/Commands/RescheduleInterview/RescheduleInterviewNotificationHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using RecruitmentNotifications.Messages;
+using RecruitmentNotifications.Servises;
+
+namespace Application.Interviews.Commands.RescheduleInterview;
+
+public class RescheduleInterviewNotificationHandler(IMessageSender sender) : INotificationHandler<InterviewRescheduledEvent>
+{
+    public Task Handle(InterviewRescheduledEvent notification, CancellationToken cancellationToken) =>
+        sender.NotifyInterviewRescheduled(notification, cancellationToken);
+}
