46d715dc081dba668e9255decb41e08f1fc73f2c
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-13 15:06:31 +0300

    Add architecture tests

---
+++ Recruitment Service/ArchitectureTests/ArchitectureTests.cs
@@ -0,0 +1,130 @@
+﻿using NetArchTest.Rules;
+using Shouldly;
+
+namespace ArchitectureTests;
+
+public class ArchitectureTests
+{
+    private const string DomainNamespace = "Domain";
+    private const string ApplicationNamespace = "Application";
+    private const string InfrastructureNamespace = "Infrastructure";
+    private const string PresentationNamespace = "Presentation";
+    private const string WebApiNamespace = "WebApi";
+
+    private const string HandlersNameEnding = "Handler";
+
+
+    [Fact]
+    public void Domain_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        //Arrange
+        var assembly = typeof(Domain.AssemblyReference).Assembly;
+
+        var otherProjects = new[]
+        {
+            ApplicationNamespace,
+            InfrastructureNamespace,
+            PresentationNamespace,
+            WebApiNamespace
+        };
+
+        //Act
+        var result = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        //Assert
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Application_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        //Arrange
+        var assembly = typeof(Application.AssemblyReference).Assembly;
+
+        var otherProjects = new[]
+        {
+            ApplicationNamespace,
+            PresentationNamespace,
+            WebApiNamespace
+        };
+
+        //Act
+        var result = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        //Assert
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Infrastructure_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        //Arrange
+        var assembly = typeof(Infrastructure.AssemblyReference).Assembly;
+
+        var otherProjects = new[]
+        {
+            PresentationNamespace,
+            WebApiNamespace
+        };
+
+        //Act
+        var result = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        //Assert
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Presentation_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        //Arrange
+        var assembly = typeof(Application.AssemblyReference).Assembly;
+
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace,
+            WebApiNamespace
+        };
+
+        //Act
+        var result = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        //Assert
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Handlers_Should_HaveDependencyOnDomainNamespace()
+    {
+        //Arrange
+        var assembly = typeof(Application.AssemblyReference).Assembly;
+
+        //Act
+        var result = Types
+            .InAssembly(assembly)
+            .That()
+            .HaveNameEndingWith(HandlersNameEnding)
+            .Should()
+            .HaveDependencyOn(DomainNamespace)
+            .GetResult();
+        //Assert
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+}
