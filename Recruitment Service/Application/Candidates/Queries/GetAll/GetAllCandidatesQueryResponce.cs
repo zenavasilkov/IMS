dbfb8dd1b2ebb4b8c1e7d3f8316d707941d991e6
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-13 14:25:32 +0300

    Add GetAll functionality

---
+++ Recruitment Service/Application/Candidates/Queries/GetAll/GetAllCandidatesQueryResponce.cs
@@ -0,0 +1,6 @@
+﻿using Application.Candidates.Queries.FindById;
+using Pagination;
+
+namespace Application.Candidates.Queries.GetAll;
+
+public sealed record GetAllCandidatesQueryResponce(PagedList<FindCandidateByIdQueryResponse> Candidates);
---
+++ Recruitment Service/Application/Employees/Queries/GetAll/GetAllEmployeesQueryResponse.cs
@@ -0,0 +1,6 @@
+﻿using Application.Employees.Queries.GetEmployeeById;
+using Pagination;
+
+namespace Application.Employees.Queries.GetAll;
+
+public sealed record GetAllEmployeesQueryResponse(PagedList<GetEmployeeByIdQueryResponse> Employees);
