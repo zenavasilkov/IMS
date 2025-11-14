cb491adea5874ae34c7eadf6a7651c930c441c26
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-14 17:19:51 +0300

    Update WebApi

---
+++ Recruitment Service/WebApi/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using static Presentation.ApiConstants.ApiConstants;
+
+namespace WebApi.Middleware;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception exception)
+        {
+            logger.LogError(exception, "An unhandled exception occurred while processing the request.");
+
+            await HandleExceptionAsync(context);
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context)
+    {
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = ContentType;
+
+        var response = new
+        {
+            Message = "An unexpected error occurred. Please try again later."
+        };
+
+        await context.Response.WriteAsJsonAsync(response);
+    }
+}
