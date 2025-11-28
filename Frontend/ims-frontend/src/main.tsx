cfd1d697b19e7f4501ccb9eaf90a4a550189b3de
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-28 23:11:50 +0300

    move strings to constants

--- Frontend/ims-frontend/src/main.tsx
+++ Frontend/ims-frontend/src/main.tsx
@@ -4,13 +4,20 @@ import './index.css'
 import App from './App.tsx'
 import { Auth0Provider } from '@auth0/auth0-react';
 
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+const scope = "openid profile email offline_access";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      domain={domain}
+      clientId={clientId}
       authorizationParams={{
         redirect_uri: globalThis.location.origin,
+          audience: audience,
+          scope: scope,
       }}
       onRedirectCallback={appState => {
         globalThis.history.replaceState(
