f019b3375dc38ff5ec10c792e56baa1b8bc3b588
zenavasilkov <zenavasilkov6@gmail.com>
2025-11-29 18:45:35 +0300

    Functioning log in version back up

--- Frontend/ims-frontend/src/App.tsx
+++ Frontend/ims-frontend/src/App.tsx
@@ -2,15 +2,26 @@ import './App.css'
 import Auth0Login from './components/Auth0Login';
 import useAuth0Interceptors from './hooks/useAuth0Interceptors';
 import { ImsApi, RecruitmentApi } from './api/axios';
+import LoginPage from './components/LoginPage/LoginPage';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { isAuthenticated, loginWithRedirect, isLoading } = useAuth0();
   useAuth0Interceptors({ axiosInstances: [ImsApi, RecruitmentApi] });
+
+  if (isLoading) {
+    return <div>Loading application...</div>;
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <Auth0Login />
-      </header>
+      {isAuthenticated ? (
+        <header className="App-header">
+          <Auth0Login />
+        </header>
+      ) : (
+        <LoginPage onLogin={loginWithRedirect} />
+      )}
     </div>
   );
 }
