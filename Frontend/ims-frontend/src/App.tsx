f7aadfc91f49eec40c17bda949f5d84aec5dc9cc
zenavasilkov <zenavasilkov6@gmail.com>
2025-12-09 00:24:13 +0300

    Improve reliability

--- Frontend/ims-frontend/src/App.tsx
+++ Frontend/ims-frontend/src/App.tsx
@@ -16,14 +16,15 @@ function App() {
   const { role, isLoadingRole } = useUserRole();
   const isAppLoading = isLoading || isTokenLoading || isLoadingRole;
   const showPageLoader = useMinLoadingTime(isAppLoading, 300);
-  const HrManagerHeader = () => <div><ManagementIcon className="App-Header-Icon" />User Management Portal</div>;
+
+  const HrManagerHeader = <div><ManagementIcon className="App-Header-Icon" />User Management Portal</div>;
 
   let contentToRender;
   let header;
 
   if (role === 'HRManager'){
       contentToRender = <UserManagementPage />;
-      header = <HrManagerHeader />
+      header = HrManagerHeader
   }
 
   if (showPageLoader) {
--- Frontend/ims-frontend/src/features/JwtDecoder.ts
+++ Frontend/ims-frontend/src/features/JwtDecoder.ts
@@ -1,7 +1,7 @@
 ﻿export const decodeJwt = (token: string) => {
     try {
         const base64Url = token.split('.')[1];
-        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const base64 = base64Url.replaceAll(/-/g, '+').replaceAll(/_/g, '/');
         const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
         }).join(''));
